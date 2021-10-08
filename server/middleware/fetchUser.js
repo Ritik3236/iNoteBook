7fc4f757e150199b5aa7b0f95a9fa8479bcd546c
Ritik3236 <ritikkushwaha1234@gmail.com>
2021-10-08 17:09:37 +0530

    jwt and middleware added

---
+++ server/middleware/fetchUser.js
@@ -0,0 +1,14 @@
+const jwt = require('jsonwebtoken');
+
+const fetchUser = (req, res, next) => {
+    const token = req.header('auth-token');
+    try {
+        const data = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        req.user = data.user;
+        next();
+    } catch (err) {
+        res.status(400).send({error : "Please Authenticate with valid jwt token"})
+    }
+}
+
+module.exports = fetchUser;
--- server/router/auth.js
+++ server/router/auth.js
@@ -1,5 +1,6 @@
 const { body, validationResult } = require('express-validator');
 const User = require('../models/userSchema');
+const fetchUser = require('../middleware/fetchUser')
 const express = require('express');
 const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
@@ -9,8 +10,9 @@ router.get('/', (req, res) => {
     res.json(req.body);
 })
 
-// ? Create a new user using Auth/createUser Api, No login Required.
-router.post('/createUser',
+// ! ROUTE 1 :
+// ? Create a new user using api/Auth/signup Api, No login Required.
+router.post('/signup',
 
     // Validation.
     body('name', 'Enter a valid name').isLength({ min: 4 }),
@@ -31,24 +33,69 @@ router.post('/createUser',
             };
             // Create a new user.
             const salt = await bcryptjs.genSalt(5);
+            console.log(salt)
             const secPass = await bcryptjs.hash(req.body.password, salt);
             user = await User.create({
                 name: req.body.name,
                 email: req.body.email,
                 password: secPass,
             })
-            const data = {
-                user: {
-                    id: user.id
-                }
-            }
+            const data = { user: { id: user.id } };
             const authToken = jwt.sign(data, process.env.JWT_SECRET_KEY);
-            console.log(authToken);
-            res.status(200).send(user)
+            res.status(201).send({ user: user, authToken: authToken });
+        } catch (err) {
+            console.log(err);
+            res.status(500).send('Some Error Occurred');
+        }
+    })
+
+//! ROUTE 2 :
+// ? Login user using api/Auth/login Api, No login Required.
+router.post('/login',
+
+    // Validation.
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Enter a valid password').exists(),
+
+    async (req, res) => {
+        // If there is any Validation error, abort the request.
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        try {
+            const { email, password } = req.body;
+            // Check if user with given Email  exists.
+            let user = await User.findOne({ email: email });
+            if (!user) {
+                return res.status(403).send("Plz try to login with correct credential");
+            };
+            // if exists then validate passwords
+            const passCompare = await bcryptjs.compare(password, user.password)
+            if (!passCompare) {
+                return res.status(403).send("Plz try to login with correct credential");
+            }
+            // generating JWT and sending
+            const data = { user: { id: user.id } }
+            const authToken = jwt.sign(data, process.env.JWT_SECRET_KEY);
+            res.status(200).send({ user: user, authToken: authToken });
         } catch (err) {
             console.log(err)
             res.status(500).send('Some Error Occurred')
         }
     })
+
+//! ROUTE 3 :
+// ? Get Logged user detail using /Auth/getUser Api, login Required.
+router.post('/getuser', fetchUser, async (req, res) => {
+    try {
+        const userId = req.user.id;
+        const user = await User.findById(userId).select('-password');
+        res.status(200).send({ user: user });
+    } catch (err) {
+        console.log(err)
+        res.status(500).send('Some Error Occurred')
+    }
+})
 
 module.exports = router;
