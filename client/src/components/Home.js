d6b31c9aa2e32eaa3f9429d2d5ac160e069b0601
Ritik3236 <ritikkushwaha1234@gmail.com>
2021-10-09 21:07:26 +0530

    ...

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,22 +3,25 @@ import Notes from './Notes';
 const Home = () => {
     return (
         <>
-            <form className="container">
-                <div className="mb-3">
-                    <label htmlFor="eie" className="form-label">Email address</label>
-                    <input type="email" autoComplete="email" className="form-control" id="eie" aria-describedby="emailHelp" />
-                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="eip" className="form-label">Password</label>
-                    <input type="password" autoComplete="current-password" className="form-control" id="eip" />
-                </div>
-                <div className="mb-3 form-check">
-                    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                    <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-                </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
-            </form>
+            <div className="container my-3">
+                <h2>Add a Note</h2>
+                <form className="my-3">
+                    <div className="mb-3">
+                        <label htmlFor="eie" className="form-label">Email address</label>
+                        <input type="email" autoComplete="email" className="form-control" id="eie" aria-describedby="emailHelp" />
+                        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="eip" className="form-label">Password</label>
+                        <input type="password" autoComplete="current-password" className="form-control" id="eip" />
+                    </div>
+                    <div className="mb-3 form-check">
+                        <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                        <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+            </div>
             <Notes />
         </>
     )
--- client/src/components/NoteItem.js
+++ client/src/components/NoteItem.js
@@ -1,22 +1,21 @@
 import React from 'react';
 
-const Notes = (props) => {
+const NoteItem = (props) => {
     const { _id, user_id, title, description, tag, date } = props.notes;
     return (
-        <> <div className="col-md-3">
-            <div className="card my-3">
-                <div className="card-body">
-                    <h5 className="card-title">{title}
-                        {/* <span className="position-absolute badge top-0 start-0 bg-dark bg-opacity-75" >{tag}</span> */}
-                    </h5>
-                    <p className="card-text">{description} Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex odit perspiciatis velit non? Asperiores consectetur, ducimus dolores maxime fugiat eligendi amet deserunt officia eveniet quisquam illum, eum molestias iure nihil minus beatae neque perferendis?</p>
-                    {/* <p className="card-text"><small className="text-muted">By {user_id} on {new Date(date).toGMTString()} </small></p> */}
-                    {/* <a href={newsUrl} target="_blank" rel="noopener noreferrer" className="btn btn-sm btn-outline-dark">Read more..</a> */}
+        <>
+            <div className="col-md-3">
+                <div className="card my-3">
+                    <div className="card-body">
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{description}</p>
+                        <i class="fa-solid fa-trash-can mx-2"></i>
+                        <i class="fa-solid fa-pen-to-square mx-2"></i>
+                    </div>
                 </div>
             </div>
-        </div>
         </>
     )
 }
 
-export default Notes
+export default NoteItem;
